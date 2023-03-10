0b6167d44d37d472da180e9dc95c342e272a8efa
elias <eliasmanica@poli.ufrj.br>
2023-03-10 15:43:20 -0300

    refactor: transition modal

--- src/components/CardModall/CardModal.js
+++ src/components/CardModall/CardModal.js
@@ -2,7 +2,37 @@ import { Container, ViewOpacity } from "./styles";
 
 import { motion } from "framer-motion";
 
-export default function CardModal({ showModal, setShowModal }) {
+export default function CardModal({ setShowModal }) {
+  const variants = {
+    open: {
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    closed: {
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
+  const variantsModal = {
+    open: {
+      scale: 1,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    closed: {
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
   return (
     <>
       <>
@@ -11,39 +41,21 @@ export default function CardModal({ showModal, setShowModal }) {
           initial={{
             opacity: 0,
           }}
-          animate={{
-            opacity: 1,
-            transition: {
-              duration: 0.3,
-            },
-          }}
-          exit={{
-            opacity: 0,
-            transition: {
-              duration: 0.3,
-            },
-          }}
+          variants={variants}
+          animate={"open"}
+          exit={"closed"}
           onClick={() => {
             setShowModal(false);
           }}
-        ></ViewOpacity>
+        />
         <Container
           as={motion.div}
           initial={{
             scale: 0,
           }}
-          animate={{
-            scale: 1,
-            transition: {
-              duration: 0.3,
-            },
-          }}
-          exit={{
-            scale: 0,
-            transition: {
-              duration: 0.3,
-            },
-          }}
+          variants={variantsModal}
+          animate={"open"}
+          exit={"closed"}
         />
       </>
     </>

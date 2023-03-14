1ac489ec07a265fbf7fc55c5fc00f38d7fd8ad0e
elias <eliasmanica@poli.ufrj.br>
2023-03-14 14:21:27 -0300

    feat: input top bar web implemented

--- src/components/InputTopBar/InputTopBar.js
+++ src/components/InputTopBar/InputTopBar.js
@@ -11,7 +11,7 @@ import {
 
 import { FaSearch } from "react-icons/fa";
 import { useEffect, useState } from "react";
-import { toolInput } from "../../services/api";
+import { findTool, toolInput } from "../../services/api";
 
 export default function InputTopBar() {
   const [isSelect, setIsSelect] = useState(false);
@@ -21,8 +21,18 @@ export default function InputTopBar() {
     const response = toolInput();
 
     setData(response);
-    console.log(response, " response");
     return response;
+  }
+
+  async function findToolByName(text) {
+    const response = await findTool(text);
+
+    if (text === "") {
+      getTools();
+      return;
+    }
+
+    setData(response.splice(0, 4));
   }
 
   useEffect(() => {
@@ -36,6 +46,9 @@ export default function InputTopBar() {
           placeholder="Buscar +80 ferramentas"
           onFocus={() => setIsSelect(true)}
           onBlur={() => setIsSelect(false)}
+          onChange={(e) => {
+            findToolByName(e.target.value);
+          }}
         />
         <Icon isSelect={isSelect}>
           <FaSearch />
@@ -43,7 +56,12 @@ export default function InputTopBar() {
         {isSelect && (
           <ContainerTool>
             {data.map((item) => (
-              <ViewTool>
+              <ViewTool
+                key={item.app_id}
+                onMouseDown={() => {
+                  window.open(item.link, "_blank").focus();
+                }}
+              >
                 <BackIcon backColor={item.color}>
                   <ImageTool src={item.icon} />
                 </BackIcon>
